43129aa24bd59839d68666607256e19c24dc9415
paulpschreiber <paulpschreiber1@gmail.com>
2020-01-29 18:59:47 -0500

    Added pathfinder files

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,142 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.VelocityMeasPeriod;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.commands.TeleopDriveCommand;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.followers.EncoderFollower;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.strykeforce.thirdcoast.swerve.SwerveDrive;
+import org.strykeforce.thirdcoast.swerve.SwerveDriveConfig;
+import org.strykeforce.thirdcoast.swerve.Wheel;
+import org.strykeforce.thirdcoast.telemetry.TelemetryService;
+import org.strykeforce.thirdcoast.telemetry.item.TalonItem;
+
+public class DriveSubsystem extends SubsystemBase {
+
+  private static final double ROBOT_LENGTH = 1.0;
+  private static final double ROBOT_WIDTH = 1.0;
+  private static final double MAX_VELOCITY = 1000;
+  private static final double MAX_ACCELERATION = 1.0;
+  private static final double MAX_JERK = 1.0;
+
+  private final SwerveDrive swerve = configSwerve();
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  private EncoderFollower follower = new EncoderFollower();
+  private Trajectory trajectoryGenerated;
+
+  public DriveSubsystem() {
+    swerve.setFieldOriented(true);
+    setDefaultCommand(new TeleopDriveCommand());
+  }
+
+  public void drive(double forward, double strafe, double yaw) {
+    swerve.drive(forward, strafe, yaw);
+  }
+
+  public void zeroGyro() {
+    AHRS gyro = swerve.getGyro();
+    gyro.setAngleAdjustment(0);
+    double adj = gyro.getAngle() % 360;
+    gyro.setAngleAdjustment(-adj);
+    logger.info("resetting gyro: ({})", adj);
+  }
+
+  private Wheel[] getWheels() {
+    TalonSRXConfiguration azimuthConfig = new TalonSRXConfiguration();
+    azimuthConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.CTRE_MagEncoder_Relative;
+    azimuthConfig.continuousCurrentLimit = 10;
+    azimuthConfig.peakCurrentDuration = 0;
+    azimuthConfig.peakCurrentLimit = 0;
+    azimuthConfig.slot0.kP = 10.0;
+    azimuthConfig.slot0.kI = 0.0;
+    azimuthConfig.slot0.kD = 100.0;
+    azimuthConfig.slot0.kF = 0.0;
+    azimuthConfig.slot0.integralZone = 0;
+    azimuthConfig.slot0.allowableClosedloopError = 0;
+    azimuthConfig.motionAcceleration = 10_000;
+    azimuthConfig.motionCruiseVelocity = 800;
+    azimuthConfig.velocityMeasurementWindow = 64;
+    azimuthConfig.voltageCompSaturation = 12;
+
+    TalonSRXConfiguration driveConfig = new TalonSRXConfiguration();
+    driveConfig.continuousCurrentLimit = 40;
+    driveConfig.peakCurrentDuration = 45;
+    driveConfig.peakCurrentLimit = 40;
+    driveConfig.slot0.integralZone = 1000;
+    driveConfig.slot0.maxIntegralAccumulator = 150_000;
+    driveConfig.slot0.allowableClosedloopError = 0;
+    driveConfig.velocityMeasurementPeriod = VelocityMeasPeriod.Period_100Ms;
+    driveConfig.velocityMeasurementWindow = 64;
+    driveConfig.voltageCompSaturation = 12;
+
+    TelemetryService telemetryService = RobotContainer.TELEMETRY;
+    telemetryService.stop();
+
+    Wheel[] wheels = new Wheel[4];
+
+    for (int i = 0; i < 4; i++) {
+      TalonSRX azimuthTalon = new TalonSRX(i);
+      azimuthTalon.configAllSettings(azimuthConfig);
+      azimuthTalon.enableCurrentLimit(true);
+      azimuthTalon.enableVoltageCompensation(true);
+      azimuthTalon.setNeutralMode(NeutralMode.Coast);
+
+      TalonSRX driveTalon = new TalonSRX(i + 10);
+      driveTalon.configAllSettings(driveConfig);
+      driveTalon.setNeutralMode(NeutralMode.Brake);
+      driveTalon.enableCurrentLimit(true);
+      driveTalon.enableVoltageCompensation(true);
+
+      telemetryService.register(new TalonItem(azimuthTalon, "Azimuth " + i));
+      telemetryService.register(new TalonItem(driveTalon, "Drive " + (i + 10)));
+    }
+    telemetryService.start();
+
+    return wheels;
+  }
+
+  private SwerveDrive configSwerve() {
+    SwerveDriveConfig config = new SwerveDriveConfig();
+    config.length = ROBOT_LENGTH;
+    config.width = ROBOT_WIDTH;
+    config.wheels = getWheels();
+    config.gyro = new AHRS(SPI.Port.kMXP);
+    config.gyroLoggingEnabled = true;
+    config.summarizeTalonErrors = false;
+
+    return new SwerveDrive(config);
+  }
+  // Pathfinder Stuff
+  public void calculateTrajctory() {
+    Waypoint[] points = Constants.AUTO_PATH;
+    Trajectory.Config trajectoryConfig =
+        new Trajectory.Config(
+            Trajectory.FitMethod.HERMITE_CUBIC,
+            Trajectory.Config.SAMPLES_HIGH,
+            0.05,
+            MAX_VELOCITY,
+            MAX_ACCELERATION,
+            MAX_JERK);
+    trajectoryGenerated = Pathfinder.generate(points, trajectoryConfig);
+  }
+
+  public void startPath() {
+    follower.setTrajectory(trajectoryGenerated);
+    int talonPosition = swerve.getWheels()[0].getDriveTalon().getSelectedSensorPosition();
+    follower.configureEncoder(talonPosition, 4096, 0.0635);
+    follower.configurePIDVA(1.0, 0.0, 0.0, 1 / MAX_VELOCITY, 0);
+  }
+}
